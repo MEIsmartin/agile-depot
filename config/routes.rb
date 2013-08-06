5c0c494443b91ca59ea5273be3f88d98a3ac765e
Shaun <smartin@meiresearch.org>
2013-08-06 13:02:39 -0500

    Store with caching

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Depot::Application.routes.draw do
   resources :products
 
+  root "store#index", as: 'store'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
