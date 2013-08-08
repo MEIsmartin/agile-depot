00edcc6dcabe59f7e0e88dae31cd9095ae7f10fb
Shaun <smartin@meiresearch.org>
2013-08-08 16:19:51 -0500

    Finish checkout.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Depot::Application.routes.draw do
+  resources :orders
+
   resources :line_items
 
   resources :carts
 
-  resources :products
+  resources :products do
+    get :who_bought, on: :member
+  end
 
   root "store#index", as: 'store'
 
